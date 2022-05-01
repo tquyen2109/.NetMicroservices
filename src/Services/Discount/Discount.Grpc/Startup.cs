0caddf7407eff074beee7ab0542a30cbc96dab7a
tquyen2109 <t.quyen1997@yahoo.com>
2022-05-01 14:08:36 -0400

    Add Discount proto file + Discount service

--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +19,8 @@ namespace Discount.Grpc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<IDiscountRepositories, DiscountRepositories>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,8 +35,7 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
-
+                endpoints.MapGrpcService<DiscountService>();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
